4055b6d0b18f8ecbfe47f1c233d0c6e48dedcde2
Soh Yu Ming <sohymg@gmail.com>
2015-04-08 18:11:04 +0800

    Added js specs for pages

--- spec/controller_helper.rb
+++ spec/controller_helper.rb
@@ -1,7 +1,5 @@
 require 'rails_helper'
-require 'jasmine_rails/save_fixture'
 
 RSpec.configure do |config|
   config.include Devise::TestHelpers, type: :controller
-  config.include JasmineRails::SaveFixture
 end
