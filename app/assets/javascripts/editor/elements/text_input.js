6c7b97d5e7f062bde743e390c41c0a75b7671d55
Nikhil Choudhary <nikhil.choudhary94@gmail.com>
2015-06-10 18:55:06 +0800

    Text_Input of one line (for forms) added

---
+++ app/assets/javascripts/editor/elements/text_input.js
@@ -0,0 +1,30 @@
+//= require ./element
+
+Elements.Text_Input = function() {
+    Elements.Element.call(this);
+
+    this.prefill = Elements.Text.lorem_ipsum_array.slice(0, 3).join(" ");
+}
+
+Elements.Text_Input.prototype = Object.create(Elements.Element.prototype); 
+Elements.Text_Input.prototype.constructor = Elements.Text_Input;
+
+Elements.Text_Input.prototype.destroy = function() {
+    Elements.Element.prototype.destroy.call(this);
+    this.properties = [];
+}
+
+Elements.Text_Input.prototype.render = function() {
+    if(this.html === null) {
+        this.html = Util.clone_template('#element_textinput_template');
+        this.html.data('element', this);
+     	console.log(this.html);
+        this.html.children('input:eq(0)').innerHTML = this.prefill;
+
+        this.hitarea = this.html.children('.element-hitarea:eq(0)')
+            .mousedown(function(e) { return Editor.mousedown_element(this, e); }.bind(this))
+            .mouseup(function(e) { return Editor.mouseup_element(this, e); }.bind(this));
+
+    }
+    return this.html;
+}
