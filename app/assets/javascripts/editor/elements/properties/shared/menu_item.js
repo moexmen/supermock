1adc2294668a8688a520ada3a9cec530e0bac2b0
Nikhil Choudhary <nikhil.choudhary94@gmail.com>
2015-06-25 19:11:27 +0800

    Abstract class principle for Property.MenuItem adhered to

--- app/assets/javascripts/editor/elements/properties/shared/menu_item.js
+++ app/assets/javascripts/editor/elements/properties/shared/menu_item.js
@@ -30,15 +30,6 @@ Elements.Property.MenuItem.prototype.visible = function() {
 }
 
 Elements.Property.MenuItem.prototype.render = function() {
-    if(this.html === null) {
-        this.html = Util.clone_template('#element_menu_item_template');
-        this.html.addClass('disabled');
-
-        this.hitarea = this.html.children('a:eq(0)');
-        this.hitarea.text('No properties');
-    }
-
-    return this.html;
 }
 
 Elements.Property.MenuItem.prototype.unrender = function() {
