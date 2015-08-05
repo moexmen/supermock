c4b64e5a9bd840edacd6bcfd3296fe37199bf163
Nikhil Choudhary <nikhil.choudhary94@gmail.com>
2015-08-05 15:36:42 +0800

    Mass tabbing in console supported

--- app/assets/javascripts/editor/console.js
+++ app/assets/javascripts/editor/console.js
@@ -26,44 +26,87 @@ Console.keydown = function(e) {
         Console.refresh();
     }
     else if(e.keyCode === 9) { // tab was pressed
-        // get caret position/selection
-        var start = this.selectionStart;
-        var end = this.selectionEnd;
-
-        // set textarea value to: text before caret + tab + text after caret
-        var value = Console.render().val();
-        Console.render().val(value.substring(0, start)
-            + "\t"
-            + value.substring(end));
-
-        // put caret at right position again (add one for the tab)
-        this.selectionStart = this.selectionEnd = start + 1;
-
-        // prevent the focus lose
         e.preventDefault();
+
+        if(this.selectionStart == this.selectionEnd){ //no highlighting of text
+            Console.tab_one_line(this);
+        }
+        else { //indent many lines together
+            Console.tab_many_lines(this);
+        }
     }
     else if(e.keyCode === 13) { // enter pressed
         e.preventDefault();
 
-        this.selectionStart = Console.shift_to_previous_indent(this.selectionStart);
-        this.selectionEnd = this.selectionStart;
+        Console.shift_to_previous_indent(this);
     }
 };
 
-Console.shift_to_previous_indent = function(start) {
+Console.tab_many_lines = function(textarea_console) {
+    var start = textarea_console.selectionStart;
+    var end = textarea_console.selectionEnd;
+
     var value = Console.render().val();
+    var first_third = value.substring(0, start);
+    var second_third = value.substring(start, end);
+    var last_third = value.substring(end);
+    var final_value = '';
+    var counter = 1;
+
+    var first_third_array = first_third.split('\n');
+    var current_selected_line = first_third_array.pop();
+    first_third_array.push('\t' + current_selected_line);
+    final_value = first_third_array.join('\n');
+
+    var second_third_array = second_third.split('\n');
+    $.each(second_third_array, function(i, line){
+        if(i == 0) {
+            final_value += line;
+        }
+        else {
+            final_value += '\n\t' + line;
+            counter += 1;
+        }
+    });
+
+    final_value += last_third;
+
+    Console.render().val(final_value);
+    textarea_console.selectionStart = start + 1;
+    textarea_console.selectionEnd = end + counter;
+};
+
+Console.tab_one_line = function(textarea_console) {
+    var start = textarea_console.selectionStart;
+    var end = textarea_console.selectionEnd;
+
+    // set textarea value to: text before caret + tab + text after caret
+    var value = Console.render().val();
+    Console.render().val(value.substring(0, start)
+        + "\t"
+        + value.substring(end));
+
+    // put caret at right position again (add one for the tab)
+    textarea_console.selectionStart = textarea_console.selectionEnd = start + 1;
+};
+
+Console.shift_to_previous_indent = function(textarea_console) {
+    var start = textarea_console.selectionStart;
+    var value = Console.render().val();
+
     var first_half = value.substring(0, start);
     var second_half = value.substring(start);
 
     var indent_level = first_half.split('\n').pop().match(/^\t*/)[0].length;
 
-    first_half += '\n'; //new line
+    first_half += '\n'; 
     for (i=0; i<indent_level; i++) {
         first_half += '\t';
     }
 
     Console.render().val(first_half + second_half);
-    return start + indent_level + 1;
+
+    textarea_console.selectionStart = textarea_console.selectionEnd = start + indent_level + 1;
 };
 
 Console.keyup = function(e) {
